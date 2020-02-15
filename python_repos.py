859b7d6188e2dab24ab40a994eb45a2aa33b178c
Deon Choi <deon.choi@gmail.com>
2020-02-14 17:36:44 -0800

    api call

---
+++ python_repos.py
@@ -0,0 +1,7 @@
+import requests
+
+#make API call and store response.
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers = headers)
+print(f'Status code: {r.status_code}')
